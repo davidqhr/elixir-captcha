aaebc0e271c436d60af6a5bcbdab947084cc31fe
david <davidqhr@gmail.com>
2017-03-27 17:37:06 +0800

    add ex_doc

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Captcha.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
   defp aliases do
