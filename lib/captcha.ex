c3c895a8f5fb50fc218506361a7133599a682ee9
Ace Wen <ace.wen@edragonconnect.com>
2019-07-02 16:14:31 +0800

    Fix priv path

--- lib/captcha.ex
+++ lib/captcha.ex
@@ -1,6 +1,6 @@
 defmodule Captcha do
   def get(timeout \\ 1_000) do
-    case System.cmd(Path.join(Path.dirname(__ENV__.file), "../priv/captcha"), []) do
+    case System.cmd(Application.app_dir(:captcha, "priv/captcha"), []) do
       {data, 0} ->
         <<text::bytes-size(5), img::binary>> = data
         {:ok, text, img}
